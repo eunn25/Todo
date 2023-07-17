2e1331c633e790827baa56c053417e3cd322e582
eunn25 <rose092504@naver.com>
2023-07-17 18:31:33 +0900

    index.js에 AuthProvider 추가

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "./context/AuthProvider";
 
 const root = createRoot(document.getElementById("root"));
 
 setInterval(() => {
   root.render(
     <React.StrictMode>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </React.StrictMode>
   );
 });
