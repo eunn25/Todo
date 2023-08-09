abaabbac289984026d66b03741d07935c87bee79
eunn25 <rose092504@naver.com>
2023-08-09 18:36:10 +0900

    오류 해결

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
 
 function App() {
   return (
-    <Router>
-      <main className="App">
-        <Switch>
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </main>
-    </Router>
+    <main className="App">
+      <Register />
+    </main>
   );
 }
 
