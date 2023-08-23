435047dcb8d62b356df765cde8b3460e6d1b2140
eunn25 <rose092504@naver.com>
2023-08-23 16:23:30 +0900

    기본 페이지 Login 설정, 페이지 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </main>
   );
 }
